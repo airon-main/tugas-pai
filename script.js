1474ad030c025aa75961402e0be0924ff9503190
airon-main <airon.pribadi@gmail.com>
2023-03-11 09:41:56 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+function countdown() {
+    const countDate = new Date("March 22, 2023 00:00:00").getTime();
+    const now = new Date().getTime();
+    const gap = countDate - now;
+
+    //How Time Works (only nerds)
+    const second = 1000;
+    const minute = second * 60;
+    const hour = minute * 60;
+    const day = hour * 24;
+
+    //Calculate the Damn Date
+    const textDay = Math.floor(gap / day);
+    const textHour = Math.floor((gap % day) / hour);
+    const textMinute = Math.floor((gap % hour) / minute);
+    const textSecond = Math.floor((gap % minute) / second);
+
+    document.querySelector('.day').innerHTML = textDay;
+    document.querySelector('.hour').innerHTML = textHour;
+    document.querySelector('.minute').innerHTML = textMinute;
+    document.querySelector('.second').innerHTML = textSecond;
+}
+
+setInterval(countdown, 1000);
